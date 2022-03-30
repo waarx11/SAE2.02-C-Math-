f97ac74dbd6e9db99ac3c507f59ce8c2252ef3f8
elandre4 <eloan.andre@etu.uca.fr>
2022-03-30 19:14:59 +0000

    Update matrice.cpp

--- src/matrice.cpp
+++ src/matrice.cpp
@@ -1,12 +1,30 @@
+/**
+ * @file matrice.cpp
+ * @author Eloan ANDRE
+ * @brief Implemente un réseau à l'aide d'une classe Matrice contenant un tableau 2 dimensions de booleen (True quand le craft existe)
+ * @version 0.1
+ * @date 2022-03-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include "matrice.hpp"
 using namespace std;
 
+/**
+ * @brief Constructeur d'un reseau de type matrice
+ * 
+ */
 Matrice::Matrice(){}
 
-
+/**
+ * @brief Ajout un objet au tableau des objets
+ * 
+ * @param o string 
+ */
 void Matrice::ajouterObjet(string o){
     // tableauObjet.push_back(o);
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
@@ -17,13 +35,14 @@ void Matrice::ajouterObjet(string o){
     }
     tableauObjet.push_back(o);
 
-    //Erreur lorsque on esssaye d'ajouter une colonne donc on fait a l'initialisation mais taille fixe
-    // for(int i=0;i<int(tableauObjet.size())-1;i++){ 
-        // matriceCraft[i][int(tableauObjet.size())-1]=0;
-        // matriceCraft[int(tableauObjet.size())-1][i]=0; 
-    // }
 }
 
+
+/**
+ * @brief Supprime un objet dans le tableau d'objet et les craft le concernant dans la matrice
+ * 
+ * @param o string
+ */
 void Matrice::supprimerObjet(string o){
     int pos=0;
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
@@ -39,6 +58,13 @@ void Matrice::supprimerObjet(string o){
     tableauObjet.erase(tableauObjet.begin()+pos);
 }
 
+
+/**
+ * @brief Ajoute un craft dans la matrice à la place de o1->o2
+ * 
+ * @param o1 string
+ * @param o2 string
+ */
 void Matrice::ajouterCraft(string o1, string o2){
     int pos1=0,pos2=0;
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
@@ -63,6 +89,13 @@ void Matrice::ajouterCraft(string o1, string o2){
     matriceCraft[pos1][pos2]=true;
 }
 
+
+/**
+ * @brief Supprime un craft à la matrice
+ * 
+ * @param o1 string
+ * @param o2 string
+ */
 void Matrice::supprimerCraft(string o1, string o2){
     int pos1=0,pos2=0;
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
@@ -86,6 +119,12 @@ void Matrice::supprimerCraft(string o1, string o2){
     matriceCraft[pos1][pos2]=false;
 }
 
+
+/**
+ * @brief Affiche les craft directs d'un objet
+ * 
+ * @param o string
+ */
 void Matrice::afficherCraft(string o){
     int pos=0,pos2;
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
@@ -109,7 +148,11 @@ void Matrice::afficherCraft(string o){
     cout<<endl;
 }
 
-
+/**
+ * @brief Affiche l'ensemble des craft disponible à partir d'un objet avec un affichage de l'objet de base
+ * 
+ * @param o string
+ */
 void Matrice::afficherCraftsuccessifGlob(string o){
     int pos=0,pos2;
     vector<string> visited;
@@ -142,6 +185,14 @@ void Matrice::afficherCraftsuccessifGlob(string o){
     cout<<endl;
 }
 
+
+/**
+ * @brief Affiche l'ensemble des craft disponible à partir d'un objet
+ * 
+ * @param o string
+ * @param visited vector<string>
+ * @return vector<string> le tableau des objets déjà visité qui ne doivent plus être parcourus 
+ */
 vector<string> Matrice::afficherCraftsuccessif(string o,vector<string> visited){
     int pos=0,pos2;
     for(vector<string>::iterator it = tableauObjet.begin(); it != tableauObjet.end(); it++){
